8e20f4fa249fe2511b9dad1ae45e9b6fa7e622ae
jakazzy <jakazzy@gmail.com>
2019-10-13 10:28:12 +0000

    add new recipes

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,24 @@
 const express = require("express");
-const app = express();
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
+const app = express();
+app.use(bodyParser.json());
+mongoose.connect(
+  "mongodb+srv://jida:stbf6CKIQoicpx7r@cluster0-d9wpy.mongodb.net/test?retryWrites=true&w=majority"
+);
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
 app.use((req, res) => {
   res.end("server running");
 });
---
+++ backend/recipe.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+const recipeSchema = mongoose.Schema({
+  title: { type: String, required: true },
+  ingredients: { type: String, required: true },
+  instructions: { type: String, required: true },
+  timeToPrepare: { type: Number, required: true },
+  difficulty: { type: Number, required: true }
+});
+
+module.exports = mongoose.model("Recipes", recipeSchema);
