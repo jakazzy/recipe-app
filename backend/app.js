5f24476094e5f9016339d6124772c0ea90c4d6ca
jakazzy <jakazzy@gmail.com>
2019-10-13 10:36:00 +0000

    data model

--- backend/app.js
+++ backend/app.js
@@ -3,22 +3,37 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 
 const app = express();
-app.use(bodyParser.json());
-mongoose.connect(
-  "mongodb+srv://jida:stbf6CKIQoicpx7r@cluster0-d9wpy.mongodb.net/test?retryWrites=true&w=majority"
-);
+const Recipe = require("./recipe");
+
+mongoose
+  .connect(
+    "mongodb+srv://jida:stbf6CKIQoicpx7r@cluster0-d9wpy.mongodb.net/test?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("Connection with MongoDB successful");
+  })
+  .catch(error => {
+    console.log("unable to connect with MongoDB atlas");
+    console.log(error);
+  });
+
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
+
   res.setHeader(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
   );
+
   res.setHeader(
     "Access-Control-Allow-Methods",
     "GET, POST, PUT, DELETE, PATCH, OPTIONS"
   );
   next();
 });
+
+app.use(bodyParser.json());
+
 app.use((req, res) => {
   res.end("server running");
 });
