f83f6d67dc5bffc8d8bae10aa252460e2f8daae7
jakazzy <jakazzy@gmail.com>
2019-10-13 12:14:13 +0000

    add data

--- backend/app.js
+++ backend/app.js
@@ -3,28 +3,24 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 
 const app = express();
-const Recipe = require("./recipe");
+const Recipe = require("./models/recipe");
 
 mongoose
   .connect(
     "mongodb+srv://jida:stbf6CKIQoicpx7r@cluster0-d9wpy.mongodb.net/test?retryWrites=true&w=majority"
   )
-  .then(() => {
-    console.log("Connection with MongoDB successful");
-  })
-  .catch(error => {
-    console.log("unable to connect with MongoDB atlas");
-    console.log(error);
+  .then(() => console.log("Connection with mongoDB successful"))
+  .catch(err => {
+    console.log("unable to connect to mongodb atlas");
+    console.error(err);
   });
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
-
   res.setHeader(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
   );
-
   res.setHeader(
     "Access-Control-Allow-Methods",
     "GET, POST, PUT, DELETE, PATCH, OPTIONS"
@@ -34,7 +30,27 @@ app.use((req, res, next) => {
 
 app.use(bodyParser.json());
 
-app.use((req, res) => {
-  res.end("server running");
+app.post("/api/recipes", (req, res, next) => {
+  const recipe = new Recipe({
+    title: req.body.title,
+    ingredients: req.body.ingredients,
+    instructions: req.body.instructions,
+    timeToPrepare: req.body.timeToPrepare,
+    difficulty: req.body.difficulty,
+    userId: req.body.userId
+  });
+  console.log(recipe, "this is the recipe");
+  recipe
+    .save()
+    .then(() => {
+      return res.status(201).json({
+        message: "Post saved successfully"
+      });
+    })
+    .catch(error => {
+      return res.status(400).json({
+        error: error
+      });
+    });
 });
 module.exports = app;
--- backend/models/recipe.js
+++ backend/models/recipe.js
@@ -3,8 +3,8 @@ const recipeSchema = mongoose.Schema({
   title: { type: String, required: true },
   ingredients: { type: String, required: true },
   instructions: { type: String, required: true },
-  timeToPrepare: { type: Number, required: true },
-  difficulty: { type: Number, required: true }
+  timeToPrepare: { type: Number },
+  difficulty: { type: Number, min: 1, max: 5, required: true }
 });
 
-module.exports = mongoose.model("Recipes", recipeSchema);
+module.exports = mongoose.model("Recipe", recipeSchema);
